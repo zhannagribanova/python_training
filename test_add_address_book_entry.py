241e276624fdda90e89fa28725946a19df2f8588
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-15 00:03:54 +0300

    Переход теста на py.test

--- test_add_address_book_entry.py
+++ test_add_address_book_entry.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
-class TestAddAddressBookEntry(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
     
-    def test_add_address_book_entry(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname="Oleg", middlename="Ivan", lastname="Kolesnikov", nickname="olegkolesnikov",
+def test_add_address_book_entry(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="Oleg", middlename="Ivan", lastname="Kolesnikov", nickname="olegkolesnikov",
                             photo="C:\\Users\\j.gribanova\\Pictures\\photo_2019-01-28_23-36-11.jpg", title="Title",
                             company="Apple", address_company="USA California", telephone_home="13-13-13",
                             telephone_mobile="89111111111", telephone_work="2-12-12", fax="123-123-123-123",
@@ -19,10 +21,4 @@ class TestAddAddressBookEntry(unittest.TestCase):
                             homepage="https://test.ru", bday="1", bmonth="January", byear="1990", aday="1",
                             amonth="January", ayear="2030", home_address="Russia, Saint-Petersburg", home="13",
                             notes="Oleg is a good man"))
-        self.app.logout()
-    
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
