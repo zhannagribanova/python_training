14f34be6d0d853bd405d7d318623e751f2e52997
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-15 00:54:00 +0300

    Выделен класс для работы с контактами

---
+++ fixture/contact.py
@@ -0,0 +1,89 @@
+from selenium.webdriver.support.ui import Select
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_add_new_page()
+        # fill contact form, name input
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        # photo upload
+        wd.find_element_by_name("photo").clear()
+        wd.find_element_by_name("photo").send_keys(contact.photo)
+        # information about work
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address_company)
+        # telephones
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.telephone_home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.telephone_mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.telephone_work)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        # e-mails and homepage
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email1)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        # birthday
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        # anniversary
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
+        wd.find_element_by_name("aday").click()
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element_by_name("amonth").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        # address
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.home_address)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.home)
+        # notes
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_home_page()
+
+    def open_add_new_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
--- test/test_add_address_book_entry.py
+++ test/test_add_address_book_entry.py
@@ -13,12 +13,12 @@ def app(request):
     
 def test_add_address_book_entry(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Oleg", middlename="Ivan", lastname="Kolesnikov", nickname="olegkolesnikov",
-                            photo="C:\\Users\\j.gribanova\\Pictures\\photo_2019-01-28_23-36-11.jpg", title="Title",
-                            company="Apple", address_company="USA California", telephone_home="13-13-13",
-                            telephone_mobile="89111111111", telephone_work="2-12-12", fax="123-123-123-123",
-                            email1="test1@test.ru", email2="test2@test.ru", email3="test3@test.ru",
-                            homepage="https://test.ru", bday="1", bmonth="January", byear="1990", aday="1",
-                            amonth="January", ayear="2030", home_address="Russia, Saint-Petersburg", home="13",
-                            notes="Oleg is a good man"))
+    app.contact.create(Contact(firstname="Oleg", middlename="Ivan", lastname="Kolesnikov", nickname="olegkolesnikov",
+                               photo="C:\\Users\\j.gribanova\\Pictures\\photo_2019-01-28_23-36-11.jpg", title="Title",
+                               company="Apple", address_company="USA California", telephone_home="13-13-13",
+                               telephone_mobile="89111111111", telephone_work="2-12-12", fax="123-123-123-123",
+                               email1="test1@test.ru", email2="test2@test.ru", email3="test3@test.ru",
+                               homepage="https://test.ru", bday="1", bmonth="January", byear="1990", aday="1",
+                               amonth="January", ayear="2030", home_address="Russia, Saint-Petersburg", home="13",
+                               notes="Oleg is a good man"))
     app.session.logout()
