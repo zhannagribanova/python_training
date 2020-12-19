d1d395efd94e4f37efd2ac1ed15086357deb0186
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-19 16:20:49 +0300

    Изменено название теста

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 
 
-def test_modification_first_group(app):
+def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="Edit_New_group", header="Edit_Logo", footer="Edit_Comment"))
     app.session.logout()
