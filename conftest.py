3321846cf4277b107bae1422447224542280240c
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-19 14:38:16 +0300

    Создаем одну фикстуру на всю сессию. Добавляем ожидание появления элемента ввода логина после логаута

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 from fixture.application import Application
 import pytest
 
-@pytest.fixture()
+
+@pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
