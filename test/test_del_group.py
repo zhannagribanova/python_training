5ff4c930518040e8cfbd2637e12b169dbdc6e324
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-19 14:10:50 +0300

    Сделана общая для всех тестов фикстура

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,12 +1,3 @@
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
