a8fc0721e7e518fea665d724a7b8ac8557610e28
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-24 22:43:25 +0300

    Добавлена проверка наличия хотя бы одного элемента при удалении групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -113,3 +113,8 @@ class ContactHelper:
     def open_add_new_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
