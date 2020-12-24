4181014c7be4ea2f5a636609351c663d890caeb1
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2020-12-24 22:47:04 +0300

    Добавлена проверка наличия хотя бы одного элемента при редактировании групп и контактов

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='Test'))
     app.group.edit_first_group(Group(name="New group"))
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='Test'))
     app.group.edit_first_group(Group(header="New header"))
 
 
 def test_edit_first_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='Test'))
     app.group.edit_first_group(Group(footer="New footer"))
