902b96a361f632acd73ebcd21ccaee00497d370f
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2021-01-02 20:21:14 +0300

    В тест удаления групппы добавлена проверка равенства самих списков групп, в дополнение к проверке их длины

--- model/group.py
+++ model/group.py
@@ -3,4 +3,10 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
-        self.id = identifier
+        self.identifier = identifier
+
+    def __repr__(self):
+        return "%s:%s" % (self.identifier, self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.identifier == other.identifier
