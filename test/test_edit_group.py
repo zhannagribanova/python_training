d7013a98854a80f4d912001504fe6c6fa0cfbe6e
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2021-01-02 22:27:45 +0300

    Добавлено сравнение отсортированных списков групп в тест изменения группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,11 +5,18 @@ def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='Test'))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="New group"))
+    group = Group(name="New group")
+    # remember the identifier
+    group.identifier = old_groups[0].identifier
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    # replace
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+"""
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name='Test'))
@@ -26,3 +33,4 @@ def test_edit_first_group_footer(app):
     app.group.edit_first_group(Group(footer="New footer"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+"""
