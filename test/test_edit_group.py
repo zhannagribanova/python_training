6f2a235e53e27f2aa288e2c2f6a547b06ef1d61d
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2021-01-03 14:32:37 +0300

    Проверяем кол-во групп перед тем как получить список новых групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -10,8 +10,8 @@ def test_edit_first_group_name(app):
     # remember the identifier
     group.identifier = old_groups[0].identifier
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     # replace
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
