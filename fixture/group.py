089e7535a6a4d8a633a3af41373f823cdf354dfc
Zhanna Gribanova <zzzgggrrriii@gmail.com>
2021-01-04 15:31:01 +0300

    Доработаны тесты удаления и редактирования групп, на выбор случайной группы

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,6 @@
 from model.group import Group
 
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -24,21 +25,33 @@ class GroupHelper:
 
     def delete_first_group(self):
         wd = self.app.wd
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def edit_first_group(self, new_group_data):
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,19 +1,21 @@
 from random import randint
 from model.group import Group
+from random import randrange
 
 
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='Test'))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="New group "+str(randint(1, 1000)))
     # remember the identifier
-    group.identifier = old_groups[0].identifier
-    app.group.edit_first_group(group)
+    group.identifier = old_groups[index].identifier
+    app.group.edit_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
     # replace
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
