3d468fcee410b518bcd08cb19504d9f58f184926
zhannagribanova <zzzgggrrriii@gmail.com>
2021-01-16 00:36:57 +0300

    Убран лишний комментарий

--- test/test_add_address_book_entry.py
+++ test/test_add_address_book_entry.py
@@ -37,30 +37,3 @@ def test_add_address_book_entry(app, contact):
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(Contact(firstname=contact.firstname, lastname=contact.lastname, identifier=contact.identifier))
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-"""
-    contact = Contact(firstname="Oleg",
-                      middlename="Ivan",
-                      lastname="Kolesnikov",
-                      nickname="olegkolesnikov",
-                      photo="C:\\Users\\j.gribanova\\Pictures\\photo_2019-01-28_23-36-11.jpg",
-                      title="Title",
-                      company="Apple",
-                      address_company="USA California",
-                      telephone_home="13-13-13",
-                      telephone_mobile="89111111111",
-                      telephone_work="2-12-12",
-                      fax="123-123-123-123",
-                      email1="test1@test.ru",
-                      email2="test2@test.ru",
-                      email3="test3@test.ru",
-                      homepage="https://test.ru",
-                      bday="1", bmonth="January",
-                      byear="1990", aday="1",
-                      amonth="January",
-                      ayear="2030",
-                      home_address="Russia, Saint-Petersburg",
-                      telephone_secondary="13",
-                      notes="Oleg is a good man")
-"""
