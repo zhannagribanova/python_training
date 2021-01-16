6a28b81a06634cea78b48f8c0d71a98164fa3e2c
zhannagribanova <zzzgggrrriii@gmail.com>
2021-01-17 00:13:02 +0300

    Вынесли создание тестовых данных в отдельный пакет, перенесен код создания групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbol = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-]
-
-
-def random_string(prefix, maxlen):
-    symbol = string.ascii_letters + string.digits + " "
-    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
+from data.add_group import constant as testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[str(x) for x in testdata])
