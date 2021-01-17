3de8571e570534e77502e593a36faab83470b381
zhannagribanova <zzzgggrrriii@gmail.com>
2021-01-17 13:55:12 +0300

    Добавлена загрузка тестовых данных в формате json (и декодирование их обратно в набор объектов python)

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import importlib
 import os.path
+import jsonpickle
 
 fixture = None
 target = None
@@ -40,9 +41,17 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
-            testdata = load_form_module(fixture[5:])
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
-def load_form_module(module):
+def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
