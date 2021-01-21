f6208ae77ff3ed3490bbd45803f96f59dd915b94
zhannagribanova <zzzgggrrriii@gmail.com>
2021-01-21 23:39:38 +0300

    Добавлен метод получения списка контактов из БД

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,14 +1,11 @@
-import pymysql.cursors
+from fixture.db import DbFixture
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             database='addressbook')
+db = DbFixture(host='localhost', name='addressbook', user='root', password='')
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
-    connection.close()
+    db.destroy()
