2bd8d74eae07837562edb7d755f924fd0e83454a
zhannagribanova <zzzgggrrriii@gmail.com>
2021-01-24 19:25:18 +0300

    Реализовали методы получение из БД контактов входящих или не входящих в группу

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,9 +1,10 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host='localhost', name='addressbook', user='root', password='')
 
 try:
-    l = db.get_contact_list()
+    l = db.contacts_not_in_group(Group(identifier='102'))
     for item in l:
         print(item)
     print(len(l))
