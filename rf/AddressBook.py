1c32aa6a94852d5ed58903199f71e5a02d988587
zhannagribanova <zzzgggrrriii@gmail.com>
2021-02-11 00:15:35 +0300

    Реализованы тесты с использованием Robot Framework для добавления и удаления контакта

---
+++ rf/AddressBook.py
@@ -0,0 +1,43 @@
+import json
+import os.path
+from fixture.db import DbFixture
+from fixture.application import Application
+from model.group import Group
+
+
+class AddressBook:
+
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+
+    def __init__(self, config="target.json", browser="chrome"):
+        self.browser = browser
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", config)
+        with open(config_file) as f:
+            self.target = json.load(f)
+
+    def init_fixtures(self):
+        web_config = self.target['web']
+        self.fixture = Application(browser=self.browser, base_URL=web_config['baseURL'])
+        self.fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+        db_config = self.target['db']
+        self.dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'],
+                                   password=db_config['password'])
+
+    def destroy_fixtures(self):
+        self.dbfixture.destroy()
+        self.fixture.destroy()
+
+    def new_group(self, name, header, footer):
+        return Group(name=name, header=header, footer=footer)
+
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def create_group(self, group):
+        self.fixture.group.create(group)
+
+    def group_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
+
+    def delete_group(self, group):
+        self.fixture.group.delete_group_by_id(group.identifier)
